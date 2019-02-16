51f0e81c17392e80814196945845408429273b65
Claudio Garaycochea <computadoraweb@gmail.com>
2019-02-16 19:26:00 -0300

    Properties reader

--- src/components/modules/moduleSocialNetwork/ModuleSocialNetwork.js
+++ src/components/modules/moduleSocialNetwork/ModuleSocialNetwork.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 import './ModuleSocialNetwork.css';
 
 class ModuleSocialNetwork extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      moduleSrc: null,
+    };
+    //this.handleMenuBtn = this.handleMenuBtn.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('**************** componentDidMount: ModuleSocialNetwork:', this.props.properties);
+    this.setState({
+      properties: this.props.properties
+    })
+  }
+
   render() {
-    return (
-      <div>
-        Follow US
-        <a href="https://nba.com" className="btn" >Facebook</a>
-        <a href="https://nba.com" className="btn" >Twitter</a>
-      </div>
-    );
+    if(this.state.properties===true) {
+      return (
+        <div>
+          <div>Module Social Network Properties:</div> 
+          <div>
+            <input type="text" onChange={()=>{}} />
+          </div>
+          <div>
+            <a href="https://nba.com" className="btn" >Save</a>
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          Follow US
+          <a href="https://nba.com" className="btn" >Facebook</a>
+          <a href="https://nba.com" className="btn" >Twitter</a>
+        </div>
+      );
+    }
   }
 }
 
