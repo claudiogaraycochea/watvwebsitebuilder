9492c5bdf7877742c79dc6c5e21af9a6b1b8df12
Claudio Garaycochea <computadoraweb@gmail.com>
2019-03-01 23:53:11 -0300

    Loading fixed

--- src/commons/Commons.js
+++ src/commons/Commons.js
@@ -9,5 +9,13 @@ export const ejfunc = () => {
 }
 
 export const LoadingSpinner = (value) => (
-  (value) ? <div className="loading-spinner">Loading...</div> : null
+  (value) ? <div className="loading-spinner"></div> : null
+);
+
+export const Notification = (value, typeClass) => (
+  (value) ? <div className={`notification ${typeClass}`}>{value}</div> : null
+);
+
+export const redirectionBySession = () => (
+  (sessionStorage.getItem('user_token')==='') ? console.log('No session') : null
 );
