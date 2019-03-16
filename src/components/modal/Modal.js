c51aa580449dbe18330df32971147d01d088496e
Claudio Garaycochea <computadoraweb@gmail.com>
2019-03-15 23:49:43 -0300

    Main CSS fixed

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -7,9 +7,10 @@ class Modal extends Component {
     return (
       <div className="modal-wrapper">
         <div className="modal-box">
-          <div className="modal-header">{this.props.title} <button onClick={this.props.closeModal} className="btn small">Close</button></div>  
+          <div className="modal-header">{this.props.title} <i className="icon-close" onClick={this.props.closeModal}/></div>  
           <div className="modal-content">{this.props.children}</div>
-          <div className="modal-footer"><button onClick={this.props.closeModal} className="btn btn-primary">Ok</button></div>
+          <div className="modal-footer"></div>
+          {/*<div className="modal-footer"><button onClick={this.props.closeModal} className="btn btn-primary">Ok</button></div>*/}
         </div>
       </div>
     );
