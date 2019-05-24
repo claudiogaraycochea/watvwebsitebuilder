c2eea6640eca07e046f7b50079206218bbe1679b
Claudio Garaycochea <computadoraweb@gmail.com>
2019-05-24 10:58:30 -0300

    Home fixed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css';
+import logo from '../../assets/logo-watv.svg';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Footer from "../footer/Footer";
@@ -14,7 +15,8 @@ class Home extends Component {
     return <div className="tertiary-style">
 				<div className="container padding-20 center">
           <div className="center-wrapper">
-						Internet and Television Together
+            <div className="logo-wrapper"><img src={logo} className="logo logo-large" alt="" /></div>
+						<h2 className="slogan">Internet and Television Together</h2>
 					</div>
 				</div>
 				<Footer className="footer" />
