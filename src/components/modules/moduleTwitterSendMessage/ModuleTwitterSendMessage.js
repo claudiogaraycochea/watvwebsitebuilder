2a451931e5a55310e24a296214a654b54b2efae5
Claudio Garaycochea <computadoraweb@gmail.com>
2019-06-25 00:10:53 -0300

    Twitter

--- src/components/modules/moduleTwitterSendMessage/ModuleTwitterSendMessage.js
+++ src/components/modules/moduleTwitterSendMessage/ModuleTwitterSendMessage.js
@@ -59,6 +59,7 @@ class ModuleTwitterSendMessage extends Component {
       return (
         <div className="mod-facebook-send-message mod-row">
           <div className="mod-row-small">
+            Twitter
             <textarea className="mod-inp" value={this.props.moduleSrc.text}/>
           </div>
           <div className="mod-row-small">
